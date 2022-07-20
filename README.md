9fb47776a5d730e041205c1e3320c852dec030b9
Daiki Kusuyama (UX490UAR) <daiki.milky.kinako@gmail.com>
2022-07-20 17:58:02 +0900

    [dev]initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mute/Unmute Script with Bluetooth Button
+
+## 参考
+https://monomonotech.jp/kurage/raspberrypi/daiso_btshutter.html
---
+++ mute.py
@@ -0,0 +1,23 @@
+import evdev
+import subprocess
+import time
+
+while True:
+    try:
+        # ls /dev/input でevent番号要確認
+        device = evdev.InputDevice('/dev/input/event18')
+        print(device)
+
+        for event in device.read_loop():
+            if event.type == evdev.ecodes.EV_KEY:
+                if event.value == 1: # 0:KEYUP, 1:KEYDOWN
+                    print(event.code)
+                    if event.code == evdev.ecodes.KEY_VOLUMEUP:
+                        subprocess.Popen(['amixer', 'set', 'Capture','cap'])
+                    if event.code == evdev.ecodes.KEY_ENTER:
+                        subprocess.Popen(['amixer', 'set', 'Capture','nocap'])
+
+    except Exception as e:
+        print(e)
+        print('Retry...')
+        time.sleep(1)
